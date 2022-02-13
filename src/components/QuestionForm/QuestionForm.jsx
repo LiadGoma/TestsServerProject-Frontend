945bbcf7f999ff8da5ab52c8509b9675599642cc
LiadGoma <liadg99@gmail.com>
2022-02-13 23:11:58 +0200

    added states to form with a lot of bugs

--- src/components/QuestionForm/QuestionForm.jsx
+++ src/components/QuestionForm/QuestionForm.jsx
@@ -1,50 +1,109 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Answer from '../Answer/Answer';
 import TextEditor from '../TextEditor/TextEditor';
 import "./QuestionForm.css";
 
 
 function QuestionForm() {
+    const [isMultiChoice, setIsMultiChoice] = useState(false);
+    const [isHorizontal, setIsHorizontal] = useState(true);
+    const [questionText, setQuestionText] = useState("");
+    const [extraContent, setExtraContent] = useState("");
+    const [answers, setAnswers] = useState([]);
+    const [tags, setTags] = useState([]);
 
+
+    useEffect(() => {
+        const tempAnswers = [
+            { id: 0, content: "", isCorrect: false },
+            { id: 1, content: "", isCorrect: false },
+            { id: 2, content: "", isCorrect: false },
+            { id: 3, content: "", isCorrect: false }
+        ]
+        setAnswers(tempAnswers);
+    }, [])
+
+
+    const selectQuestionTypeChangeHandler = (e) => {
+        setIsMultiChoice(e.target.value);
+    }
+    const selectIsHorizontalChangeHandler = (e) => {
+        setIsHorizontal(e.target.value);
+    }
+    const tagsChangeHandler = (e) => {
+        const value = e.target.value;
+        let tempTags = value.split(",");
+        tempTags = tempTags.map(tag => tag.trim());
+        setTags(tempTags);
+    }
+    const questionContentChangeHandler = (value) => {
+        setQuestionText(value);
+    }
+    const extraContentChangeHandler = (value) => {
+        setExtraContent(value);
+    }
+    const addAnswerClickHandler = () => {
+        const tempAnswers = [...answers, { content: "", isCorrect: false }];
+        setAnswers(tempAnswers);
+    }
+    const changeAnswer = (id, content, isCorrect) => {
+        const tempAnswers = [...answers];
+        tempAnswers[id].content = content;
+        tempAnswers[id].isCorrect = isCorrect;
+        console.log(tempAnswers);
+        setAnswers(tempAnswers);
+    }
+    const handleSaveClick=()=>{
+        console.log(isMultiChoice);
+        console.log(isHorizontal);
+        console.log(questionText);
+        console.log(extraContent);
+        console.log(answers);
+        console.log(tags);
+    }
     return (
         <div className='form'>
             <div className='field'>
-                <label>Field</label>
-                <select>
-                    <option>Single Choice Question</option>
-                    <option>Multi Choice Question</option>
+                <label>Question Type</label>
+                <select onChange={selectQuestionTypeChangeHandler}>
+                    <option value={false} selected>Single Choice Question</option>
+                    <option value={true}>Multi Choice Question</option>
                 </select>
             </div>
             <div className='field'>
-                <label>Question Type</label>
-                <TextEditor height={200} />
+                <label>Question Text</label>
+                <TextEditor height={200} changeHandler={questionContentChangeHandler} />
             </div>
             <div className='field'>
                 <label>Text Below Question</label>
-                <TextEditor height={200} />
+                <TextEditor height={200} changeHandler={extraContentChangeHandler} />
             </div>
 
             <div className='field'>
                 <label>Possible Answers</label>
                 <div className='answers'>
-                    <Answer />
-                    <Answer />
-                    <Answer />
-                    <button className='answerBtn'>ADD AN ANSWER</button>
+                    {answers.map((answer) => {
+                        return <Answer isMultiChoice={isMultiChoice} changeAnswer={changeAnswer} id={answer.id} />
+                    })}
+                    <button className='answerBtn' onClick={addAnswerClickHandler}>ADD AN ANSWER</button>
                 </div>
 
             </div>
 
             <div className='field'>
                 <label>Answers Layout</label>
-                <input type="checkbox" id="horizontal" />
+                <input type="radio" onChange={selectIsHorizontalChangeHandler} name="layout" value={true} id="horizontal" />
                 <label for="horizontal">Horizontal</label>
-                <input type="checkbox" id="vertical" />
+                <input type="radio" onChange={selectIsHorizontalChangeHandler} name="layout" value={false} id="vertical" />
                 <label for="vertical">Vertical</label>
             </div>
             <div className='field'>
                 <label>Tags</label>
-                <input></input>
+                <input onChange={tagsChangeHandler}></input>
+            </div>
+            <div className='bottomBtns'>
+                <button className='formBtn'>SHOW</button>
+                <button className='formBtn green' onClick={handleSaveClick}>SAVE</button>
             </div>
 
         </div>
--- src/components/TextEditor/TextEditor.jsx
+++ src/components/TextEditor/TextEditor.jsx
@@ -1,34 +1,38 @@
 import { Editor } from '@tinymce/tinymce-react';
 import React, { useRef } from 'react'
 
-function TextEditor({height}) {
-    const editorRef = useRef(null);
-    const log = () => {
-        if (editorRef.current) {
-            console.log(editorRef.current.getContent());
-        }
-    };
+function TextEditor({ height, changeHandler }) {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  const handleChange = (e) => {
+    changeHandler(editorRef.current.getContent());
+  }
   return (
     <>
-       <Editor
-         onInit={(evt, editor) => editorRef.current = editor}
-         initialValue="Type something"
-         init={{
-           height: height,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount'
-           ],
-           toolbar: 'undo redo | formatselect | ' +
-           'bold italic backcolor | alignleft aligncenter ' +
-           'alignright alignjustify | bullist numlist outdent indent | ' +
-           'removeformat | help',
-           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-         }}
-       />
-     </>
+      <Editor
+        onInit={(evt, editor) => editorRef.current = editor}
+        onChange={handleChange}
+        initialValue="Type something"
+        init={{
+          height: height,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+    </>
   )
 }
 
